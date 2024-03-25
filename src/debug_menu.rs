8b191b38232a83d0678ed035b1d3146b2c4774cd
StackDoubleFlow <ojaslandge@gmail.com>
2024-03-25 00:25:28 -0500

    Show camera pitch and yaw in debug menu

--- src/debug_menu.rs
+++ src/debug_menu.rs
@@ -5,11 +5,25 @@ use crate::McCamera;
 
 fn show_gui(mut contexts: EguiContexts, camera_query: Query<&Transform, With<McCamera>>) {
     let camera_transform = camera_query.single();
+    let (yaw, pitch, _) = camera_transform.rotation.to_euler(EulerRot::YXZ);
+
     egui::Window::new("Debug Info").show(contexts.ctx_mut(), |ui| {
-        ui.label(format!(
-            "Camera Position: {:?}",
-            camera_transform.translation
-        ));
+        egui::Grid::new("debug_info_grid")
+            .num_columns(2)
+            .striped(true)
+            .show(ui, |ui| {
+                ui.label("Camera Position");
+                ui.label(format!("{:?}", camera_transform.translation));
+                ui.end_row();
+
+                ui.label("Camera Pitch");
+                ui.label(format!("{:?}", pitch.to_degrees()));
+                ui.end_row();
+
+                ui.label("Camera Yaw");
+                ui.label(format!("{:?}", yaw.to_degrees()));
+                ui.end_row();
+            });
     });
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod cli;
 mod debug_menu;
 mod resources;
 
-use bevy::{prelude::*, window::CursorGrabMode};
-use bevy::window::PrimaryWindow;
+use bevy::prelude::*;
+use bevy::window::{CursorGrabMode, PrimaryWindow};
 use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
 use color_eyre::Result;
 use debug_menu::McDebugMenuPlugin;
@@ -22,17 +22,21 @@ fn setup(mut commands: Commands) {
         });
 }
 
-fn mouse_grab(keyboard_input: Res<ButtonInput<KeyCode>>, mut fly_camera: Query<&mut FlyCamera>, mut window: Query<&mut Window, With<PrimaryWindow>>) {
+fn mouse_grab(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut fly_camera: Query<&mut FlyCamera>,
+    mut window: Query<&mut Window, With<PrimaryWindow>>,
+) {
     let mut window = window.single_mut();
     let mut fly_camera = fly_camera.single_mut();
 
     let escape_pressed = keyboard_input.just_pressed(KeyCode::Escape);
     let t_pressed = keyboard_input.just_pressed(KeyCode::KeyT);
-    if !fly_camera.enabled && escape_pressed  {
+    if !fly_camera.enabled && escape_pressed {
         fly_camera.enabled = true;
         window.cursor.grab_mode = CursorGrabMode::Locked;
         window.cursor.visible = false;
-    } else if fly_camera.enabled && (t_pressed || escape_pressed){
+    } else if fly_camera.enabled && (t_pressed || escape_pressed) {
         fly_camera.enabled = false;
         window.cursor.grab_mode = CursorGrabMode::None;
         window.cursor.visible = true;
